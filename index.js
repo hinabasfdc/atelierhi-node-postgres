3ed985b4650caea0bbebcb43d67d83184981771a
Hiroyuki Inaba <hinaba@salesforce.com>
2018-06-14 16:46:13 +0900

    update ERROR handling

--- index.js
+++ index.js
@@ -11,33 +11,45 @@ const server = http.createServer((req, httpRes) => {
   httpRes.statusCode = 200;
   httpRes.setHeader('Content-Type', 'text/html');
 
+  //DB接続を生成
   const client = new Client({
     connectionString: DATABASE_URL,
   });
   client.connect();
 
+  //クエリー文字列にテーブル名を追加
   let q = "SELECT * FROM " + TABLE_NAME;
   client.query(q, (err, pgRes) => {
-    let o = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"></head><body><table>';
+    if (err) {
+      //エラーを画面に表示
+      let o = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"></head><body><p>';
+      o += err.stack;
+      o += "</p></body></html>";
+      httpRes.end(o);
+    } else {
+      let o = '<!DOCTYPE html><html lang="ja"><head><meta charset="UTF-8"></head><body><table>';
 
-    //ヘッダー行を作成
-    o += "<tr>";
-    pgRes.fields.forEach(element => {
-      o += "<th>" + element.name + "</th>";
-    });
-    o += "</tr>";
-
-    //各行を作成
-    pgRes.rows.forEach(element => {
+      //ヘッダー行を作成
       o += "<tr>";
-      for (let k in element) {
-        o += "<td>" + element[k] + "</td>";
-      };
+      pgRes.fields.forEach(element => {
+        o += "<th>" + element.name + "</th>";
+      });
       o += "</tr>";
-    });
-    o += "</table></body></html>";
 
-    httpRes.end(o);
+      //各行を作成
+      pgRes.rows.forEach(element => {
+        o += "<tr>";
+        for (let k in element) {
+          o += "<td>" + element[k] + "</td>";
+        };
+        o += "</tr>";
+      });
+      o += "</table></body></html>";
+
+      //画面表示
+      httpRes.end(o);
+    }
+    //DB接続をクローズ
     client.end();
   });
 });
